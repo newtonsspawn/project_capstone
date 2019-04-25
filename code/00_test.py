c38cb99c6ddd7c29cc98ea79b3333adb9438911e
Kenny Evans <newtonsspawn@gmail.com>
2019-04-24 19:02:33 -0700

    Got AWS working but not all modules are running.

---
+++ code/00_test.py
@@ -0,0 +1,27 @@
+import wave
+# import matplotlib.pyplot as plt
+import pandas as pd
+# import librosa
+
+curated_df = pd.read_csv('../data/train_curated.csv')
+print(curated_df.head())
+print(f'Shape: {curated_df.shape}')
+print('\n')
+
+print(f"{len(curated_df['labels'].unique())} unique labels")
+print('\n')
+
+
+noisy_df = pd.read_csv('../data/train_noisy.csv')
+print(noisy_df.head())
+print(f'Shape: {noisy_df.shape}')
+print('\n')
+
+print(f"{len(noisy_df['labels'].unique())} unique labels")
+print('\n')
+
+# f = wave.open('../data/train_curated/0006ae4e.wav')
+# print(f.getparams())
+# f.close()
+
+# plt.plot([1, 2, 3, 4, 5], [6, 7, 8, 9, 10])
