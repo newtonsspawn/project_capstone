ef2274206d76aab4f0f5e5b71a6be88dbc00fa19
Kenny Evans <newtonsspawn@gmail.com>
2019-05-15 15:26:06 -0700

    Did some thangs... Great things... Glorious things!

--- code/00j_results.py
+++ code/00j_results.py
@@ -34,13 +34,15 @@ precisions = sorted(precision_dict.items(),
 average_precision = np.average([item[1] for item in precisions])
 print(average_precision)
 
+print(precision_dict['Fart'])
+
 best_X = [item[1] for item in precisions[:10]][::-1]
 best_Y = [item[0] for item in precisions[:10]][::-1]
 
 plt.figure(1, figsize=(8, 5))
 plt.barh(best_Y, best_X)
 plt.title('Top 10 Performing Sounds by Precision Metric')
-save_fig('best_performers_plot', tight_layout=True)
+# save_fig('best_performers_plot', tight_layout=True)
 
 
 worst_X = [item[1] for item in precisions[-10:]]
@@ -49,4 +51,4 @@ worst_Y = [item[0] for item in precisions[-10:]]
 plt.figure(2, figsize=(8, 5))
 plt.barh(worst_Y, worst_X)
 plt.title('Worst 10 Performing Sounds by Precision Metric')
-save_fig('worst_performers_plot', tight_layout=True)
+# save_fig('worst_performers_plot', tight_layout=True)
